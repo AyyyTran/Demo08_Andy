d9acd5b9ffe8e66845053115496f537833d8bc7b
Andy Tran <AyyyTran@gmail.com>
2021-10-26 12:21:05 -0700

    lots of changes

--- scripts/firebaseAPI.js
+++ scripts/firebaseAPI.js
@@ -1,13 +1,11 @@
 // Your web app's Firebase configuration
-var  firebaseConfig = {
-    apiKey: ,
-    authDomain: ",
-    projectId: ,
-    storageBucket: ,
-    messagingSenderId: ,
-    appId: 
-  };
-  
+var firebaseConfig = {
+  apiKey: "AIzaSyA-qSxE3AL9CMwH2gEibll74DalVGoXLKw",
+  authDomain: "demo08-andy.firebaseapp.com",
+  projectId: "demo08-andy",
+  storageBucket: "demo08-andy.appspot.com",
+  messagingSenderId: "697817748302",
+  appId: "1:697817748302:web:2229b24db12590d358841f",
+};
 
-  const app = firebase.initializeApp(firebaseConfig);
-  
+const app = firebase.initializeApp(firebaseConfig);
